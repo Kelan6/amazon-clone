f2e74c3a21f271419b91a91ae6608ba791629001
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2023-01-28 11:34:26 -0700

    test

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ function App() {
   return (
     <div className="app">
       <h1> Lets build this Store 🫰</h1>
-      
+      <h2> trying to check github sync with prior project</h2>
     </div>
   );
 }
