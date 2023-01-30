01d21403cf57ca84311660ae1984b22d680f6122
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2023-01-30 16:12:44 -0700

    home row product one complete

--- src/Product.js
+++ src/Product.js
@@ -13,10 +13,11 @@ function Product() {
                 <div className='product_rating'>
                     <p> ⭐️ </p>
                     <p> ⭐️ </p>
-                    <p> ⭐️</p>
-                    <p> ⭐️</p>
+                    <p> ⭐️ </p>
+                    <p> ⭐️ </p>
                 </div>
                 <img src='https://m.media-amazon.com/images/I/61l80KEogWL.__AC_SX300_SY300_QL70_FMwebp_.jpg' />
+                <button className='product_button'> Add to Basket </button>
             </div>
         </div>
     )
