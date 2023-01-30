18bab3e984ea5a094f504b9a0ef794b59c3dfbb2
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2023-01-30 14:12:20 -0700

    sync proper, working on project

--- src/Home.js
+++ src/Home.js
@@ -9,8 +9,7 @@ function Home() {
           className="home_image"
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
           alt=""
-        />
-        testing this 
+        /> 
       </div>
     </div>
     );
