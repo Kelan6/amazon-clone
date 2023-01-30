ca58a39f4dcef4d7e2b61bc5b259e86c26b98065
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2023-01-30 16:00:54 -0700

    added lamar jackson punko

--- src/Home.js
+++ src/Home.js
@@ -10,10 +10,10 @@ function Home() {
           className="home_image"
           src="https://images-eu.ssl-images-amazon.com/images/G/02/digital/video/merch2016/Hero/Covid19/Generic/GWBleedingHero_ENG_COVIDUPDATE__XSite_1500x600_PV_en-GB._CB428684220_.jpg"
           alt=""
-        /> 
+        />
       </div>
       <div className="home_row">
-        <Product/>
+        <Product />
       </div>
       <div className="home_row">
         {/* product */}
@@ -24,7 +24,7 @@ function Home() {
         {/* product */}
       </div>
     </div>
-    );
+  );
 };
 
 export default Home
--- src/Product.js
+++ src/Product.js
@@ -2,18 +2,24 @@ import React from 'react'
 import './Product.css'
 
 function Product() {
-  return (
-    <div className='product'>
-        <div className='product_info'>
-            <p> Example product </p>
-            <p className='product_price'>
-                <small>$</small>
-                <strong>29.99</strong>
-            </p>
+    return (
+        <div className='product'>
+            <div className='product_info'>
+                <p> Funko POP! NFL: Lamar Jackson </p>
+                <p className='product_price'>
+                    <small>$</small>
+                    <strong>19.99</strong>
+                </p>
+                <div className='product_rating'>
+                    <p> ⭐️ </p>
+                    <p> ⭐️ </p>
+                    <p> ⭐️</p>
+                    <p> ⭐️</p>
+                </div>
+                <img src='https://m.media-amazon.com/images/I/61l80KEogWL.__AC_SX300_SY300_QL70_FMwebp_.jpg' />
+            </div>
         </div>
-
-    </div>
-  )
+    )
 }
 
 export default Product
