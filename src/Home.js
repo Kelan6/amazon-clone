2e8659d3e569c95bb2554542f00b824eb9633c7e
Kelan6 <101379206+Kelan6@users.noreply.github.com>
2023-01-31 10:40:10 -0700

    snowboard and first row added

--- src/Home.js
+++ src/Home.js
@@ -12,14 +12,13 @@ function Home() {
           alt=""
         />
         <div className="home_row">
-        <Product />
-        <Product />
-      </div>
-      <div className="home_row">
-        <Product/>
-      </div>
-      <div className="home_row">
-      </div>
+          <Product title='Funko POP! NFL: Lamar Jackson' price={18.99} image='https://m.media-amazon.com/images/I/61l80KEogWL.__AC_SX300_SY300_QL70_FMwebp_.jpg' rating={5} />
+          <Product title='Mens 2023 Snowtrooper Snowboard' price={599.95} image='https://m.media-amazon.com/images/I/5164ZYvl1QL._AC_SX679_.jpg' rating={5}/>
+        </div>
+        <div className="home_row">
+        </div>
+        <div className="home_row">
+        </div>
       </div>
     </div>
   );
--- src/Product.js
+++ src/Product.js
@@ -1,22 +1,21 @@
 import React from 'react'
 import './Product.css'
 
-function Product() {
+function Product({ title, image, price, rating }) {
     return (
         <div className='product'>
             <div className='product_info'>
-                <p> Funko POP! NFL: Lamar Jackson </p>
+                <p> {title} </p>
                 <p className='product_price'>
                     <small>$</small>
-                    <strong>19.99</strong>
+                    <strong>{price}</strong>
                 </p>
                 <div className='product_rating'>
-                    <p> ⭐️ </p>
-                    <p> ⭐️ </p>
-                    <p> ⭐️ </p>
-                    <p> ⭐️ </p>
+                    {Array(rating).fill().map((_, i) => (
+                        <p> ⭐️ </p>
+                    ))}
                 </div>
-                <img src='https://m.media-amazon.com/images/I/61l80KEogWL.__AC_SX300_SY300_QL70_FMwebp_.jpg' />
+                <img className='product_image' src={image} alt='' />
                 <button className='product_button'> Add to Basket </button>
             </div>
         </div>
